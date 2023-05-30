5a656f5c4464bc8e5d37cb59179317b56c18002a
zanozano <zanozanozano@gmail.com>
2023-05-30 22:30:36 +0700

    ULTIMA UPDATE

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,19 +9,28 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # POST /resource/sign_in
-   def create
-     super
-   end
+  def create
+  self.resource = warden.authenticate(auth_options)
+  if resource.nil?
+    flash.now[:alert] = "Credenciales inválidas. Por favor, verifica tu correo electrónico y contraseña."
+    respond_to do |format|
+    format.turbo_stream { render turbo_stream: turbo_stream.replace('login-form', partial: '/devise/shared/alert', locals: { message: flash.now[:alert] }) }
+    format.html { render :new }
+    end
+  else
+    super
+  end
+end
 
   # DELETE /resource/sign_out
-   def destroy
+  def destroy
      super
-   end
+  end
 
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_in_params
+  def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-   end
+  end
 end
