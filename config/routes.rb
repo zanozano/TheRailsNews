a51342384b74c8b4aa1d5292287dc836a4a038ea
zanozano <zanozanozano@gmail.com>
2023-05-31 19:44:44 +0700

    update 31

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks'
   }
 
   get '/sessions/index', to: 'sessions#index', as: 'session_index'
   get '/home/index', to: 'home#index', as: 'home_index'
-  
+
   resources :articles
-  
+
   root "home#index"
 end
